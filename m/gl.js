74de0f4f05a5b097790141eb133ff1e2e7417307
Grant Custer <grantcuster@gmail.com>
2020-11-24 16:03:30 -0500

    comment out round joins

--- m/gl.js
+++ m/gl.js
@@ -44,36 +44,36 @@ export function initGL() {
       [0, 0.5, 1],
       [0, 0.5, 0],
     ]
-    for (let step = 0; step < resolution; step++) {
-      const theta0 = Math.PI / 2 + ((step + 0) * Math.PI) / resolution
-      const theta1 = Math.PI / 2 + ((step + 1) * Math.PI) / resolution
-      instanceRoundRound.push([0, 0, 0])
-      instanceRoundRound.push([
-        0.5 * Math.cos(theta0),
-        0.5 * Math.sin(theta0),
-        0,
-      ])
-      instanceRoundRound.push([
-        0.5 * Math.cos(theta1),
-        0.5 * Math.sin(theta1),
-        0,
-      ])
-    }
-    for (let step = 0; step < resolution; step++) {
-      const theta0 = (3 * Math.PI) / 2 + ((step + 0) * Math.PI) / resolution
-      const theta1 = (3 * Math.PI) / 2 + ((step + 1) * Math.PI) / resolution
-      instanceRoundRound.push([0, 0, 1])
-      instanceRoundRound.push([
-        0.5 * Math.cos(theta0),
-        0.5 * Math.sin(theta0),
-        1,
-      ])
-      instanceRoundRound.push([
-        0.5 * Math.cos(theta1),
-        0.5 * Math.sin(theta1),
-        1,
-      ])
-    }
+    // for (let step = 0; step < resolution; step++) {
+    //   const theta0 = Math.PI / 2 + ((step + 0) * Math.PI) / resolution
+    //   const theta1 = Math.PI / 2 + ((step + 1) * Math.PI) / resolution
+    //   instanceRoundRound.push([0, 0, 0])
+    //   instanceRoundRound.push([
+    //     0.5 * Math.cos(theta0),
+    //     0.5 * Math.sin(theta0),
+    //     0,
+    //   ])
+    //   instanceRoundRound.push([
+    //     0.5 * Math.cos(theta1),
+    //     0.5 * Math.sin(theta1),
+    //     0,
+    //   ])
+    // }
+    // for (let step = 0; step < resolution; step++) {
+    //   const theta0 = (3 * Math.PI) / 2 + ((step + 0) * Math.PI) / resolution
+    //   const theta1 = (3 * Math.PI) / 2 + ((step + 1) * Math.PI) / resolution
+    //   instanceRoundRound.push([0, 0, 1])
+    //   instanceRoundRound.push([
+    //     0.5 * Math.cos(theta0),
+    //     0.5 * Math.sin(theta0),
+    //     1,
+    //   ])
+    //   instanceRoundRound.push([
+    //     0.5 * Math.cos(theta1),
+    //     0.5 * Math.sin(theta1),
+    //     1,
+    //   ])
+    // }
     return {
       buffer: regl.buffer(instanceRoundRound),
       count: instanceRoundRound.length,
